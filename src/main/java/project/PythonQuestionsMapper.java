3f322150bc4ced74b085b3143343f0b105a69046
Doroshchuk <doroshchukdaria@gmail.com>
2018-10-07 17:50:34 +0300

    modified mapper

--- src/main/java/project/PythonQuestionsMapper.java
+++ src/main/java/project/PythonQuestionsMapper.java
@@ -1,68 +1,80 @@
 package project;
 
 import com.opencsv.CSVParser;
+import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class PythonQuestionsMapper extends Mapper<LongWritable, Text, Text, Text> {
     private Text link = new Text();
     private  Text outkey = new Text();
-    private static int count = 0;
 
     public void map(LongWritable key, Text text, Context context) throws IOException, InterruptedException {
-            CSVParser parser = new CSVParser();
-            String[] lines = parser.parseLineMulti(text.toString());
-            if (lines.length == 0)
-                return;
-            String id = lines[0];
-            String body = lines[lines.length - 1];
-            String rawBody = removeHTMLFromString(body.toLowerCase());
-            String bodyChars = rawBody.replaceAll("'","").replaceAll("\n", " ").replaceAll("[^a-zA-Z]", " ");
-            if(count < 5 && lines.length == 5){
-                System.out.println("Id: " + id);
-                System.out.println("OwnerUserId: " + lines[1]);
-                System.out.println("CreationDate: " + lines[2]);
-                System.out.println("Score: " + lines[3]);
-                System.out.println("Title: " + lines[4]);
-                System.out.println("Body: " + bodyChars);
-            }
-            count ++;
-            for (String word: bodyChars.split("\\s+")) {
-//                System.out.println("Id: " + id);
-//                System.out.println("Word: " + word);
-                if (word.length() > 0) {
-                    if (word.charAt(0) == 'j') {
-                        link.set(word);
-                        outkey.set(id);
-                        context.write(link, outkey);
-                    }
-                }
-            }
-    }
-
-//    public void map(Text text) throws IOException, InterruptedException {
-//        CSVParser parser = new CSVParser();
-//        String[] lines = parser.parseLineMulti(text.toString());
-//        if (lines.length == 0)
-//            return;
-//        String id = lines[0];
-//        String body = lines[5];
-//        String rawBody = removeHTMLFromString(body.toLowerCase());
-//        String bodyChars = rawBody.replaceAll("'","").replaceAll("\n", " ").replaceAll("[^a-zA-Z]", " ");
-//        for (String word: bodyChars.split("\\s+")) {
-//            System.out.println("Id: " + id);
-//            System.out.println("Word: " + word);
-//            if (word.length() > 0) {
-//                if (word.charAt(0) == 'j') {
-//                    link.set(word);
-//                    outkey.set(id);
+//        CSVParser parser = CSVParser.parse(text.toString(), CSVFormat.DEFAULT
+//                                                                .withHeader("Id",
+//                                                                            "OwnerUserId",
+//                                                                            "CreationDate",
+//                                                                            "Score",
+//                                                                            "Title",
+//                                                                            "Body")
+//                                                                .withIgnoreHeaderCase()
+//                                                                .withTrim());
+//
+//        for (CSVRecord record: parser) {
+//            if (record.size() == 0)
+//                return;
+//
+//            String id = record.get("Id");
+//            String body = record.get("Body");
+//            String rawBody = removeHTMLFromString(body.toLowerCase());
+//            String bodyChars = rawBody.replaceAll("'","").replaceAll("\n", " ").replaceAll("[^a-zA-Z]", " ");
+//
+//            if(count < 5 && record.size() > 5){
+//                System.out.println("Id: " + record.get("Id"));
+//                System.out.println("OwnerUserId: " + record.get("OwnerUserId"));
+//                System.out.println("CreationDate: " + record.get("CreationDate"));
+//                System.out.println("Score: " + record.get("Score"));
+//                System.out.println("Title: " + record.get("Title"));
+//                System.out.println("Body: " + bodyChars);
+//            }
+//            count ++;
+//
+//            for (String word: bodyChars.split("\\s+")) {
+//                if (word.length() > 0) {
+//                    if (word.charAt(0) == 'j') {
+//                        link.set(word);
+//                        outkey.set(id);
+//                        context.write(link, outkey);
+//                    }
 //                }
 //            }
 //        }
-//    }
+        CSVParser parser = new CSVParser();
+
+        String[] lines = parser.parseLineMulti(text.toString());
+        if (lines.length == 0)
+            return;
+        String id = lines[0];
+        String body = lines[lines.length - 1];
+        body = StringEscapeUtils.unescapeHtml(body.toLowerCase());
+        //removeHTMLFromString(body.toLowerCase());
+        body = body.replaceAll("'","'");
+        body = body.replaceAll("[^a-zA-Z]]", " ");
+        //body.replaceAll("'","").replaceAll("\n", " ").replaceAll("[^a-zA-Z]", " ");
+        StringTokenizer itr = new StringTokenizer(body);
+        while(itr.hasMoreTokens()){
+            String word = itr.nextToken();
+            if (word.charAt(0) == 'j') {
+                link.set(word);
+                outkey.set(id);
+                context.write(link, outkey);
+            }
+        }
+    }
 
     private String removeHTMLFromString(String textWithTML){
         String strRegEx = "<[^>]*>";
