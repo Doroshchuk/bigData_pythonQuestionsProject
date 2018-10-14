3de7081e4dce2aea6cee2cc64901430271feb1f6
Doroshchuk <doroshchukdaria@gmail.com>
2018-10-14 13:30:27 +0300

    added results files

--- src/main/java/project/PythonQuestionsMapper.java
+++ src/main/java/project/PythonQuestionsMapper.java
@@ -1,6 +1,5 @@
 package project;
 
-import com.opencsv.CSVParser;
 import in.ashwanthkumar.hadoop2.mapreduce.lib.input.ArrayListTextWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -14,28 +13,22 @@ public class PythonQuestionsMapper extends Mapper<LongWritable, ArrayListTextWri
     private  Text outkey = new Text();
 
     public void map(LongWritable key, ArrayListTextWritable text, Context context) throws IOException, InterruptedException {
-        if(key.get() > 0){
-            CSVParser parser = new CSVParser();
-
-            String[] lines = parser.parseLineMulti(text.toString());
-            if (lines.length == 0)
-                return;
-            String id = lines[0].replace("[", "");
-            String body = lines[lines.length - 1].replace("]", "");
-            body = removeHTMLFromString(body.toLowerCase()); //StringEscapeUtils.unescapeHtml(body.toLowerCase());
-            body = body.replaceAll("'","");
-            body = body.replaceAll("\n", " ");
-            body = body.replaceAll("[^a-zA-Z]", " ");
-            System.out.println("Id: " + id);
-            System.out.println("Body: " + body);
-            StringTokenizer itr = new StringTokenizer(body, ".,?\n\\s");
-            while(itr.hasMoreTokens()){
-                String word = itr.nextToken();
-                if (word.charAt(0) == 'j') {
-                    link.set(word);
-                    outkey.set(id);
-                    context.write(link, outkey);
-                }
+        ArrayListTextWritable lines = text;
+        if (lines.size() < 6)
+            return;
+        String id = lines.get(0).toString();
+        String body = lines.get(5).toString();
+        body = removeHTMLFromString(body.toLowerCase()); //StringEscapeUtils.unescapeHtml(body.toLowerCase());
+        body = body.replaceAll("'","");
+        body = body.replaceAll("\n", " ");
+        body = body.replaceAll("[^a-zA-Z]", " ");
+        StringTokenizer itr = new StringTokenizer(body);
+        while(itr.hasMoreTokens()){
+            String word = itr.nextToken();
+            if (word.charAt(0) == 'j') {
+                link.set(word);
+                outkey.set(id);
+                context.write(link, outkey);
             }
         }
     }
